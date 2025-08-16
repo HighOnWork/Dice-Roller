f4d106ff4e56e599e61c39731664956c45e8d97d
HighOnWork <highonworking@gmail.com>
2025-08-16 16:31:54 +0500

    Update main.py
    Red Added

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import random
 
+Reset = '\033[0m'
+Red = '\033[31m'
 
 num = random.random() * 6
 
 final_num = round(num, 0)
 
-print(final_num)
+print(f'Your randomised die number is {Red} {final_num} {Reset}')
